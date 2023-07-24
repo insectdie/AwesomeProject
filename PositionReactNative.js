b68073ac70fdbea239a8c14e68f90eedb65cc546
insectdie <ompusungguandry@gmail.com>
2023-07-24 23:06:13 +0700

    feat postition react native

---
+++ PositionReactNative.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import cart from './cart.png';
+
+export default function PositionReactNative() {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.cartWrapper}>
+        <Image source={cart} style={styles.iconCart} />
+        <Text style={styles.notif}>10</Text>
+      </View>
+      <Text style={styles.text}>Keranjang Belanja Anda</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {alignItems: 'center', marginTop: 50},
+  cartWrapper: {
+    borderWidth: 1,
+    borderColor: '#4398D1',
+    width: 93,
+    height: 93,
+    borderRadius: 93 / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconCart: {width: 50, height: 50},
+  text: {fontSize: 12, color: '#777777', marginTop: 8},
+  notif: {
+    fontSize: 12,
+    color: 'white',
+    backgroundColor: '#6FCF97',
+    width: 24,
+    height: 24,
+    // borderWidth: 1,
+    borderRadius: 24 / 2,
+    // borderRadius: 1,
+    padding: 4,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+});
