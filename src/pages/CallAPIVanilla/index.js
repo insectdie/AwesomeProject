0856d31eb1186adf10f015434e526c3a9c25a0a9
insectdie <ompusungguandry@gmail.com>
2023-07-28 22:32:00 +0700

    Call API vanilla

--- src/pages/CallAPIVanilla/index.js
+++ src/pages/CallAPIVanilla/index.js
@@ -1,21 +1,59 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import {Button, StyleSheet, Text, View, Image} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
 const CallAPIVanilla = () => {
-  useEffect(() => {
-    //Call Api Method Get
-    fetch('https://reqres.in/api/users?page=2')
-      .then(response => response.json())
-      .then(json => console.log(json));
+  const [dataUser, setDataUser] = useState({
+    avatar: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+  });
 
-    //Call API Method POST
+  const [dataJob, setDataJob] = useState({
+    name: '',
+    job: '',
+  });
+
+  useEffect(() => {
+    // //Call Api Method Get
+    // fetch('https://reqres.in/api/users?page=2')
+    //   .then(response => response.json())
+    //   .then(json => console.log(json));
+    // //Call API Method POST
+    // const dataForAPI = {
+    //   name: 'morpheus',
+    //   job: 'leader',
+    // };
+    // console.log('data object: ', dataForAPI);
+    // console.log('data stringfy: ', JSON.stringify(dataForAPI));
+    // fetch('https://reqres.in/api/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(dataForAPI),
+    // })
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     console.log('post response: ', json);
+    //   });
+  }, []);
+
+  const getData = () => {
+    fetch('https://reqres.in/api/users/2')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        setDataUser(json.data);
+      });
+  };
+
+  const postData = () => {
     const dataForAPI = {
-      name: 'morpheus',
-      job: 'leader',
+      name: 'Andry',
+      job: 'Programmer',
     };
 
-    console.log('data object: ', dataForAPI);
-    console.log('data stringfy: ', JSON.stringify(dataForAPI));
     fetch('https://reqres.in/api/users', {
       method: 'POST',
       headers: {
@@ -26,15 +64,33 @@ const CallAPIVanilla = () => {
       .then(response => response.json())
       .then(json => {
         console.log('post response: ', json);
+        setDataJob(json);
       });
-  }, []);
+  };
+
   return (
-    <View>
-      <Text>Call API dengan Vanila JS</Text>
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>Call API dengan Vanila JS</Text>
+      <Button title="Get Data" onPress={getData} />
+
+      <Text>Response GET DATA</Text>
+      <Image source={{uri: dataUser.avatar}} style={styles.avatar} />
+      <Text>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
+      <Text>{dataUser.email}</Text>
+
+      <Text>Response POST DATA</Text>
+      <Button title="Post Data" onPress={postData} />
+      <Text>{dataJob.name}</Text>
+      <Text>{dataJob.job}</Text>
     </View>
   );
 };
 
 export default CallAPIVanilla;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {padding: 20, marginTop: 40},
+  textTitle: {textAlign: 'center'},
+  line: {height: 2, backgroundColor: 'black', marginVertical: 20},
+  avatar: {height: 100, width: 100, borderRadius: 100},
+});
